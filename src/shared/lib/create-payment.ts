fe74e5ad641e82b472cdfd17688b5bf266435017
Vadim Firulyov <firulvv@mail.ru>
2024-11-29 03:04:03 +0400

    tttt

--- src/shared/lib/create-payment.ts
+++ src/shared/lib/create-payment.ts
@@ -23,55 +23,66 @@ export async function createPayment(details: PaymentDetails) {
   //   },
   // }));
 
-  const { data } = await axios({
-    method: "post",
-    url: "https://api.yookassa.ru/v3/payments",
-    headers: {
-      "Content-Type": "application/json",
-      "Idempotence-Key": Date.now(),
-    },
-    auth: {
-      username: process.env.YOOKASSA_STORE_ID as string,
-      password: process.env.YOOKASSA_API_KEY as string,
-    },
-    data: {
-      amount: {
-        value: details.amount,
-        currency: "RUB",
+  try {
+    const { data } = await axios({
+      method: "post",
+      url: "https://api.yookassa.ru/v3/payments",
+      headers: {
+        "Content-Type": "application/json",
+        "Idempotence-Key": Date.now(),
       },
-      confirmation: {
-        type: "redirect",
-        return_url: process.env.NEXT_PUBLIC_FRONT_URL as string,
+      auth: {
+        username: process.env.YOOKASSA_STORE_ID as string,
+        password: process.env.YOOKASSA_API_KEY as string,
       },
-      payment_method_data: {
-        type: "bank_card",
-      },
-      capture: true,
-      description: details.description,
-      metadata: {
-        ...details.metadata,
-      },
-      receipt: {
-        customer: {
-          email: "customer@example.com",
-          phone: "+79000000000",
+      data: {
+        amount: {
+          value: details.amount.toFixed(2),
+          currency: "RUB",
         },
-        items: [
-          {
-            description: "Шашлык",
-            quantity: "1.00",
-            amount: {
-              value: "165.00",
-              currency: "RUB",
-            },
-            vat_code: 1,
-            payment_mode: "full_payment",
-            payment_subject: "commodity",
+        confirmation: {
+          type: "redirect",
+          return_url: process.env.NEXT_PUBLIC_FRONT_URL as string,
+        },
+        payment_method_data: {
+          type: "bank_card",
+        },
+        capture: true,
+        description: details.description,
+        metadata: {
+          ...details.metadata,
+        },
+        receipt: {
+          customer: {
+            email: "customer@example.com",
+            phone: "+79000000000",
           },
-        ],
+          items: [
+            {
+              description: "Шашлык",
+              quantity: "1.00",
+              amount: {
+                value: "165.00",
+                currency: "RUB",
+              },
+              vat_code: 1,
+              payment_mode: "full_payment",
+              payment_subject: "commodity",
+            },
+          ],
+        },
       },
-    },
-  });
+    });
+
+    return { data };
+  } catch (error: any) {
+    console.error(
+      "Ошибка при создании платежа:",
+      error.response?.data || error.message,
+    );
+    throw error;
+  }
+
   // const { data } = await axios({
   //   method: "post",
   // url: "https://api.yookassa.ru/v3/payments",
@@ -122,8 +133,6 @@ export async function createPayment(details: PaymentDetails) {
   //     },
   //   },
   // });
-
-  return { data };
 }
 // receipt: {
 //   customer: details.customer,
