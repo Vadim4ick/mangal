016d1a40504def66c7131f1b5594eff78dcf0a1e
Vadim <a-yva-2013@mail.ru>
2024-11-28 23:48:55 +0400

    fix domains

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: [
       "16a9564f-f8ec-42ba-a998-3027aa809e50.selstorage.ru",
       "0.0.0.0",
-      "kingofmangal.ru",
+      "admin.kingofmangal.ru",
     ],
   },
 };
