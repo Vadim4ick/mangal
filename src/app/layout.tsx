ed03abbc16644b4176ab01c5b0fc6a068662fb3e
Vadim Firulyov <firulvv@mail.ru>
2024-12-06 00:02:15 +0400

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
-
+import Script from "next/script";
 import "@/shared/styles/globals.css";
 import { Footer } from "@/components/Footer";
 import { Providers } from "@/shared/providers/Providers";
@@ -14,7 +15,8 @@ const manrope = Manrope({
 
 export const metadata: Metadata = {
   title: "Царь мангал | Краснодар",
-  description: "Блюда на гриле с доставкой по Краснодару",
+  description:
+    "Закажите вкусные блюда на мангале с доставкой в Краснодаре! Мы предлагаем широкий ассортимент шашлыков, люля-кебаб, овощей и рыбы. Самовывоз: ул. Карякина, 7. Заказ по телефону +79005558383. Насладитесь настоящими кулинарными шедеврами прямо у себя дома!",
 };
 
 export default function RootLayout({
@@ -33,6 +35,36 @@ export default function RootLayout({
 
           <Footer />
         </Providers>
+
+        {/* Яндекс.Метрика */}
+        <Script id="yandex-metrika-init" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) { return; }
+          }
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+          k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(99137008, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          });`}
+        </Script>
+
+        {/* Дополнительный noscript для Яндекс.Метрики */}
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/99137008"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
