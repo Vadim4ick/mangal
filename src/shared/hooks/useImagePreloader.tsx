ca919e09e14a49cf430800139ec2ffc675b910dd
Vadim Firulyov <firulvv@mail.ru>
2024-12-12 01:57:00 +0400

    фикс изображений

---
+++ src/shared/hooks/useImagePreloader.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
+const useImagePreloader = () => {
+  const [imgSpinner, setImgSpinner] = useState(true);
+
+  const handleLoadingImageComplete = async (
+    img: React.SyntheticEvent<HTMLImageElement, Event>,
+  ) => {
+    img.currentTarget.classList.remove("opacity-0");
+    setImgSpinner(false);
+  };
+
+  return { handleLoadingImageComplete, imgSpinner };
+};
+
+export { useImagePreloader };
