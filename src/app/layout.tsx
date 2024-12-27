ea6213fc66fb49c9958d058e91df3e05c7cea3d8
Vadim Firulyov <firulvv@mail.ru>
2024-12-27 19:12:00 +0400

    изменение метрики

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,28 +38,21 @@ export default function RootLayout({
 
         {/* Яндекс.Метрика */}
         <Script id="yandex-metrika-init" strategy="afterInteractive">
-          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {
-            if (document.scripts[j].src === r) { return; }
-          }
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-          k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          {` (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','//mc.yandex.ru/metrika/tag.js', 'ym');
 
-          ym(99137008, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-          });`}
+    ym(99317501, 'init', {accurateTrackBounce:true, trackLinks:true, webvisor:true, clickmap:true, params: {__ym: {isFromApi: 'yesIsFromApi'}}});
+`}
         </Script>
 
         {/* Дополнительный noscript для Яндекс.Метрики */}
         <noscript>
           <div>
             <img
-              src="https://mc.yandex.ru/watch/99137008"
+              src="https://mc.yandex.ru/watch/99317501"
               style={{ position: "absolute", left: "-9999px" }}
               alt=""
             />
